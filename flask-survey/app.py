3bf9b13aa95c41a94fd4ec029c613619ca94ec6c
Lucas Last <lucaslast@Lucass-Air.cogeco.local>
2021-02-05 19:00:51 -0500

    part 2 of flask survey

--- flask-survey/app.py
+++ flask-survey/app.py
@@ -1,6 +1,7 @@
 # still need to make this a repository
 
 from flask import Flask, request, render_template, redirect
+from flask import session, make_response, flash
 from surveys import Survey, Question, satisfaction_survey, personality_quiz
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -13,48 +14,61 @@ debug = DebugToolbarExtension(app)
 
 survey = satisfaction_survey
 total_qs = len(survey.questions)
-responses = []
 
 
 @app.route("/")
 def home():
-
+    session['responses_key'] = "dont_skip"
+    session['q_count'] = 0
     return render_template('start.html', survey=survey)
 
 
 @app.route("/start", methods=["POST"])
 def start():
+    session['responses_key'] = []
+
     return redirect("/question/0")
 
 
-@app.route("/answer", methods=["POST"])
+@ app.route("/answer", methods=["POST"])
 def answers():
-    user_reply = request.form["answer"]
-    responses.append(user_reply)
-    count = len(responses)
-    return redirect(f"/question/{count}")
-
-
-@app.route("/question/<int:quest>")
-def questions(quest):
-    count = len(responses)
+    user_reply = request.form['answer']
+    store_session = session['responses_key']
+    session['responses_key'] = user_reply
+    store_session.append(user_reply)
+    session['responses_key'] = store_session
+    count = len(store_session)
+    session['q_count'] = count
 
     if count >= total_qs:
-        return redirect('/endsurvey')
-
-    elif count != quest:
-        return render_template(f"question.html", survey=survey, num=count)
-
+        return redirect("/endsurvey")
     else:
-        return render_template(f"question.html", survey=survey, num=quest)
+        return redirect(f"/question/{count}")
 
 
-@app.route("/endsurvey")
+@ app.route("/question/<int:quest>")
+def questions(quest):
+    num = session['q_count']
+
+    if (session['responses_key'] == "dont_skip"):
+        flash('Dont Skip Ahead!!!')
+        return redirect('/')
+
+    elif session['q_count'] != quest:
+        flash("Please Answer Questions In Order")
+        return render_template(f"question.html", survey=survey, num=num)
+
+    return render_template(f"question.html", survey=survey, num=quest)
+
+
+@ app.route("/endsurvey")
 def end_survey():
-    all_resp = responses
+    all_resp = session['responses_key']
+
     return render_template("endsurvey.html", all_resp=all_resp)
 
 
-@app.route('/clear')
+@ app.route('/clear')
 def clear_data():
+    session['responses_key'] = []
     return redirect('/')
